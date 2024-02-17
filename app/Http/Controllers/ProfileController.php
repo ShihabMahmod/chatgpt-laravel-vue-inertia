afae62a4d6315b600c1d8bd5739632ee8b11326e
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-02-17 10:58:41 +0600

    want to homepage like chatgpt but html is heard i think

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -39,13 +39,9 @@ class ProfileController extends Controller
         $request->validate([
             'password' => ['required', 'current-password'],
         ]);
-
         $user = $request->user();
-
         Auth::logout();
-
         $user->delete();
-
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
